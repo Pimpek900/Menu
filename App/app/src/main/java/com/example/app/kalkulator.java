7af3eff026a93026826e1201c574539d0b11a7c8
adireszka <bot_adi@wp.pl>
2019-06-25 14:27:41 +0200

    Next update

--- App/app/src/main/java/com/example/app/kalkulator.java
+++ App/app/src/main/java/com/example/app/kalkulator.java
@@ -3,6 +3,7 @@ package com.example.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -29,7 +30,7 @@ public class kalkulator extends AppCompatActivity {
     private final char EQU = 0;
     private Button kropka;
     private Button Enter;
-    private Button Info;
+    private TextView Info;
     private TextView wynik;
     private double val1 = Double.NaN;
     private double val2;
@@ -40,6 +41,147 @@ public class kalkulator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalkulator);
+
+        setupUPViews();
+
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Info.setText(Info.getText().toString() + "0");
+            }
+        });
+
+        jeden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Info.setText(Info.getText().toString() + "1");
+            }
+        });
+
+        dwa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Info.setText(Info.getText().toString() + "2");
+            }
+        });
+
+        trzy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Info.setText(Info.getText().toString() + "3");
+            }
+        });
+
+        cztery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Info.setText(Info.getText().toString() + "4");
+            }
+        });
+
+        piec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Info.setText(Info.getText().toString() + "5");
+            }
+        });
+
+        szesc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Info.setText(Info.getText().toString() + "6");
+            }
+        });
+
+        siedem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Info.setText(Info.getText().toString() + "7");
+            }
+        });
+
+        dziewiec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Info.setText(Info.getText().toString() + "9");
+            }
+        });
+
+        osiem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Info.setText(Info.getText().toString() + "8");
+            }
+        });
+
+        dodac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                compute();
+                ACTION = DODAWANIE;
+                wynik.setText(String.valueOf(val1) + "+");
+                Info.setText(null);
+            }
+        });
+
+        odjac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                compute();
+                ACTION = ODEJMOWANIE;
+                wynik.setText(String.valueOf(val1) + "-");
+                Info.setText(null);
+            }
+        });
+
+        razy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                compute();
+                ACTION = MNOŻENIE;
+                wynik.setText(String.valueOf(val1) + "*");
+                Info.setText(null);
+            }
+        });
+
+        podzielic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                compute();
+                ACTION = DZIELENIE;
+                wynik.setText(String.valueOf(val1) + "/");
+                Info.setText(null);
+            }
+        });
+
+        Enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                compute();
+                ACTION = EQU;
+                wynik.setText(wynik.getText().toString() + String.valueOf(val2) + "=" + String.valueOf(val1));
+                Info.setText(null);
+            }
+        });
+
+        C.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Info.getText().length() > 0)
+                {
+                    CharSequence name = Info.getText().toString();
+                    Info.setText(name.subSequence(0,name.length()-1));
+                }
+
+                else
+                    {
+                        val1 = Double.NaN;
+                        val2 = Double.NaN;
+                        Info.setText(null);
+                        wynik.setText(null);
+                    }
+            }
+        });
     }
 
     private void setupUPViews()
@@ -60,6 +202,42 @@ public class kalkulator extends AppCompatActivity {
         podzielic = (Button)findViewById(R.id.dzielenie);
         kropka = (Button)findViewById(R.id.kropka);
         Enter = (Button)findViewById(R.id.enter);
+        Info = (TextView)findViewById(R.id.Info);
         wynik = (TextView)findViewById(R.id.Wynik);
+        C = (Button)findViewById(R.id.C);
+    }
+
+    private void compute()
+    {
+        if (!Double.isNaN(val1))
+        {
+            val2 = Double.parseDouble(Info.getText().toString());
+
+            switch (ACTION)
+            {
+                case DODAWANIE:
+                    val1 = val1 + val2;
+                    break;
+
+                case ODEJMOWANIE:
+                    val1 = val1 - val2;
+                    break;
+
+                case MNOŻENIE:
+                    val1 = val1 * val2;
+                    break;
+
+                case DZIELENIE:
+                    val1 = val1 / val2;
+                    break;
+
+                case EQU:
+                    break;
+            }
+        }
+        else
+            {
+                val1 = Double.parseDouble(Info.getText().toString());
+            }
     }
 }
