b3654a5cdd4a106de8dd163fc86908028c98b643
adireszka <bot_adi@wp.pl>
2019-06-26 11:09:36 +0200

    Next update

--- App/app/src/main/java/com/example/app/kik.java
+++ App/app/src/main/java/com/example/app/kik.java
@@ -3,12 +3,147 @@ package com.example.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class kik extends AppCompatActivity {
+public class kik extends AppCompatActivity implements View.OnClickListener {
+
+    private Button[][] buttons = new Button[3][3];
+    private boolean player1Turn = true;
+    private int roundCount;
+    private int player1Points;
+    private int player2Points;
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kik);
+
+        textViewPlayer1 = findViewById(R.id.player1);
+        textViewPlayer2 = findViewById(R.id.player2);
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+
+        Button button_reset = findViewById(R.id.button_reset);
+        button_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+            if (!((Button) view).getText().toString().equals(""))
+            {
+                return;
+            }
+
+            if (player1Turn)
+            {
+                ((Button)view).setText("x");
+            }
+            else
+                {
+                    ((Button)view).setText("o");
+                }
+
+            roundCount++;
+
+            if (checkForWin())
+            {
+                if (player1Turn)
+                {
+                    player1Wins();
+                }
+                else
+                    {
+                        player2Wins();
+                    }
+            }
+            else if (roundCount == 9)
+            {
+                draw();
+            }
+            else
+                {
+                    player1Turn = !player1Turn;
+                }
+    }
+
+    private boolean checkForWin()
+    {
+        String[][] field = new String[3][3];
+        for (int i = 0; i<3; i++)
+        {
+            for (int j = 0; j<3; j++)
+            {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i<3; i++)
+        {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals(""))
+            {
+                return true;
+            }
+        }
+        for (int i = 0; i<3; i++)
+        {
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals(""))
+            {
+                return true;
+            }
+        }
+
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals(""))
+        {
+            return true;
+        }
+
+        if (field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals(""))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void player1Wins()
+    {
+
+    }
+
+    private void player2Wins()
+    {
+
+    }
+
+    private void draw()
+    {
+
     }
 }
