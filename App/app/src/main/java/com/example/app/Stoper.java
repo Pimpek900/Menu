96fbbc07140b441df0607f6b5e9403f5f51cbf3e
adireszka <bot_adi@wp.pl>
2019-06-26 15:00:17 +0200

    Next update

--- App/app/src/main/java/com/example/app/Stoper.java
+++ App/app/src/main/java/com/example/app/Stoper.java
@@ -3,12 +3,44 @@ package com.example.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Chronometer;
 
 public class Stoper extends AppCompatActivity {
+
+    private Chronometer chronometer;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stoper);
+
+        chronometer = findViewById(R.id.chronometer);
     }
+
+    public void startChronometer (View view)
+        {
+            if (!running)
+            {
+                chronometer.start();
+                running = true;
+            }
+        }
+
+
+    public void stopChronometer (View view)
+        {
+            if (running)
+            {
+                chronometer.stop();
+                running = false;
+            }
+        }
+
+
+    public void resetChronometer (View view)
+        {
+
+        }
 }
