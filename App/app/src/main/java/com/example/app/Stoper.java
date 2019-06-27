1986c32b4ab16a388a0e5ba820d9db9c69d3f5c6
adireszka <bot_adi@wp.pl>
2019-06-27 11:54:20 +0200

    Next update

--- App/app/src/main/java/com/example/app/Stoper.java
+++ App/app/src/main/java/com/example/app/Stoper.java
@@ -3,12 +3,15 @@ package com.example.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Chronometer;
+import android.widget.Toast;
 
 public class Stoper extends AppCompatActivity {
 
     private Chronometer chronometer;
+    private long pauseOffset;
     private boolean running;
 
     @Override
@@ -17,12 +20,26 @@ public class Stoper extends AppCompatActivity {
         setContentView(R.layout.activity_stoper);
 
         chronometer = findViewById(R.id.chronometer);
+        chronometer.setFormat("Time: %s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if ((SystemClock.elapsedRealtime() - chronometer.getBase()) >= 10000)
+                {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    Toast.makeText(Stoper.this, "Bing!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public void startChronometer (View view)
         {
             if (!running)
             {
+                chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
                 chronometer.start();
                 running = true;
             }
@@ -34,6 +51,7 @@ public class Stoper extends AppCompatActivity {
             if (running)
             {
                 chronometer.stop();
+                pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
                 running = false;
             }
         }
@@ -41,6 +59,7 @@ public class Stoper extends AppCompatActivity {
 
     public void resetChronometer (View view)
         {
-
+            chronometer.setBase(SystemClock.elapsedRealtime());
+            pauseOffset = 0;
         }
 }
