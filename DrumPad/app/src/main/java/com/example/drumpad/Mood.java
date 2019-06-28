1f5bbe7ab7b9d49744343ff692beb5a0b04a33ac
adireszka <bot_adi@wp.pl>
2019-06-28 10:41:24 +0200

    .

--- DrumPad/app/src/main/java/com/example/drumpad/Mood.java
+++ DrumPad/app/src/main/java/com/example/drumpad/Mood.java
@@ -10,18 +10,18 @@ import android.view.View;
 public class Mood extends AppCompatActivity {
 
     private SoundPool sp;
-    private int hip_hop01;
-    private int hip_hop02;
-    private int hip_hop03;
-    private int hip_hop04;
-    private int hip_hop05;
-    private int hip_hop06;
-    private int hip_hop07;
-    private int hip_hop08;
-    private int hip_hop09;
-    private int hip_hop10;
-    private int hip_hop11;
-    private int hip_hop12;
+    private int mood01;
+    private int mood02;
+    private int mood03;
+    private int mood04;
+    private int mood05;
+    private int mood06;
+    private int mood07;
+    private int mood08;
+    private int mood09;
+    private int mood10;
+    private int mood11;
+    private int mood12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,30 +29,30 @@ public class Mood extends AppCompatActivity {
         setContentView(R.layout.activity_drum_pad01);
 
         sp = new SoundPool(2, AudioManager.STREAM_MUSIC,0);
-        hip_hop01 = sp.load(getApplicationContext(),R.raw.hip_hop01,1);
-        hip_hop02 = sp.load(getApplicationContext(),R.raw.hip_hop02,1);
-        hip_hop03 = sp.load(getApplicationContext(),R.raw.hip_hop03,1);
-        hip_hop04 = sp.load(getApplicationContext(),R.raw.hip_hop04,1);
-        hip_hop05 = sp.load(getApplicationContext(),R.raw.hip_hop05,1);
-        hip_hop06 = sp.load(getApplicationContext(),R.raw.hip_hop06,1);
-        hip_hop07 = sp.load(getApplicationContext(),R.raw.hip_hop07,1);
-        hip_hop08 = sp.load(getApplicationContext(),R.raw.hip_hop08,1);
-        hip_hop09 = sp.load(getApplicationContext(),R.raw.hip_hop09,1);
-        hip_hop10 = sp.load(getApplicationContext(),R.raw.hip_hop10,1);
-        hip_hop11 = sp.load(getApplicationContext(),R.raw.hip_hop11,1);
-        hip_hop12 = sp.load(getApplicationContext(),R.raw.hip_hop12,1);
+        mood01 = sp.load(getApplicationContext(),R.raw.mood01,1);
+        mood02 = sp.load(getApplicationContext(),R.raw.mood02,1);
+        mood03 = sp.load(getApplicationContext(),R.raw.mood03,1);
+        mood04 = sp.load(getApplicationContext(),R.raw.mood04,1);
+        mood05 = sp.load(getApplicationContext(),R.raw.mood05,1);
+        mood06 = sp.load(getApplicationContext(),R.raw.mood06,1);
+        mood07 = sp.load(getApplicationContext(),R.raw.mood07,1);
+        mood08 = sp.load(getApplicationContext(),R.raw.mood08,1);
+        mood09 = sp.load(getApplicationContext(),R.raw.mood09,1);
+        mood10 = sp.load(getApplicationContext(),R.raw.mood10,1);
+        mood11 = sp.load(getApplicationContext(),R.raw.mood11,1);
+        mood12 = sp.load(getApplicationContext(),R.raw.mood12,1);
     }
-    public void playsound1(View view) { sp.play(hip_hop01,0.5f,0.5f,0,0,1f); }
-    public void playsound2(View view) { sp.play(hip_hop02,0.5f,0.5f,0,0,1f); }
-    public void playsound3(View view) { sp.play(hip_hop03,0.5f,0.5f,0,0,1f); }
-    public void playsound4(View view) { sp.play(hip_hop04,0.5f,0.5f,0,0,1f); }
-    public void playsound5(View view) { sp.play(hip_hop05,0.5f,0.5f,0,0,1f); }
-    public void playsound6(View view) { sp.play(hip_hop06,0.5f,0.5f,0,0,1f); }
-    public void playsound7(View view) { sp.play(hip_hop07,0.5f,0.5f,0,0,1f); }
-    public void playsound8(View view) { sp.play(hip_hop08,0.5f,0.5f,0,0,1f); }
-    public void playsound9(View view) { sp.play(hip_hop09,0.5f,0.5f,0,0,1f); }
-    public void playsound10(View view) { sp.play(hip_hop10,0.5f,0.5f,0,0,1f); }
-    public void playsound11(View view) { sp.play(hip_hop11,0.5f,0.5f,0,0,1f); }
-    public void playsound12(View view) { sp.play(hip_hop12,0.5f,0.5f,0,0,1f); }
+    public void playsound1(View view) { sp.play(mood01,0.5f,0.5f,0,0,1f); }
+    public void playsound2(View view) { sp.play(mood02,0.5f,0.5f,0,0,1f); }
+    public void playsound3(View view) { sp.play(mood03,0.5f,0.5f,0,0,1f); }
+    public void playsound4(View view) { sp.play(mood04,0.5f,0.5f,0,0,1f); }
+    public void playsound5(View view) { sp.play(mood05,0.5f,0.5f,0,0,1f); }
+    public void playsound6(View view) { sp.play(mood06,0.5f,0.5f,0,0,1f); }
+    public void playsound7(View view) { sp.play(mood07,0.5f,0.5f,0,0,1f); }
+    public void playsound8(View view) { sp.play(mood08,0.5f,0.5f,0,0,1f); }
+    public void playsound9(View view) { sp.play(mood09,0.5f,0.5f,0,0,1f); }
+    public void playsound10(View view) { sp.play(mood10,0.5f,0.5f,0,0,1f); }
+    public void playsound11(View view) { sp.play(mood11,0.5f,0.5f,0,0,1f); }
+    public void playsound12(View view) { sp.play(mood12,0.5f,0.5f,0,0,1f); }
 
 }
