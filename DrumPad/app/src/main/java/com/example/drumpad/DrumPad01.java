a1186b7a3ba1f54a2fdc95185e6de32a829e91e9
Pimpek900 <m.w.15@onet.pl>
2019-06-28 09:29:06 +0200

    kolorki zoba

---
+++ DrumPad/app/src/main/java/com/example/drumpad/DrumPad01.java
@@ -0,0 +1,15 @@
+package com.example.drumpad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrumPad01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drum_pad01);
+    }
+
+}
