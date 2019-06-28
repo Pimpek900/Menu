77cf389db2477ec94adfe5d828c2ce10f6067464
Pimpek900 <m.w.15@onet.pl>
2019-06-28 09:39:28 +0200

    tak

--- DrumPad/app/src/main/java/com/example/drumpad/MainActivity.java
+++ DrumPad/app/src/main/java/com/example/drumpad/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.drumpad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void DrumPadHipHop(View view){
+        Intent intent = new Intent(MainActivity.this, DrumPad01.class);
+        startActivity(intent);
     }
 }
